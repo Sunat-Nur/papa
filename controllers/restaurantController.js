9ea959fe25bab85a43921c297f47202d6bdfafff
Sunat_nur <uzbekistan9699@gmail.com>
2023-10-21 14:28:35 +0900

    feat: develop product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -86,12 +86,19 @@ restaurantController.logout = (req, res ) => {
     res.send("logout page");
 };
 
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+    if(req.session?.member?.mb_type === "RESTAURANT") {
+        req.member = req.session.member;
+        next();
+    } else res.json({state: "fail", message: "only authenticated members with restaurant type" })
+};
+
 
 restaurantController.checkSessions = (req, res ) => {
     if(req.session?.member) {
         res.json({state: 'succeed', data: req.session.member });
     } else {
-        res.json ({state: "fail", message: "You aren't authonticated"});
+        res.json ({state: "fail", message: "You aren't authenticated"});
     }
 };
 // agar session mavjud bolsa sessiondagi ma'lumotlarni brouserga yuborsin
--- router_bssr.js
+++ router_bssr.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router_bssr = express.Router();                   // expressni ichidan router olib chiqilyabdi
 const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/productController");
 
 
 /**********************************
@@ -27,7 +28,9 @@ router_bssr.get("/logout", restaurantController.logout);
 router_bssr.get("/check-me", restaurantController.checkSessions);
 
 router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
-
+router_bssr.post("/products/create",restaurantController.validateAuthRestaurant,
+                 productController.addNewProduct);
+router_bssr.post("products/edit/:id", productController.updateChosenProduct);
 
 
 
